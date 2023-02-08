abdbc4f54273077ac96eb9ceb3f77db0df5cc2b2
Franklin Rodriguez <franklinserif@gmail.com>
2023-02-08 06:06:12 -0400

    feat: :construction: insertion sort

---
+++ src/algorithms/insertionSort.ts
@@ -0,0 +1,14 @@
+/**
+ * Problem - Given an array of integers, sort the array
+ *
+ * const arr = [-6, 20, 8, -2, 4] => should return [-6, -2, 4, 8, 20]
+ */
+
+/**
+ * insertion Sort algorithms
+ * @param arr
+ * @returns arr
+ */
+function insertionSort(arr: number[]) {}
+
+export default insertionSort;
